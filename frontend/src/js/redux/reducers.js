09fd21d5b658b90f235489b585115cce7da08b40
Sean Jun <seanjun21@gmail.com>
2016-08-31 08:16:40 -0700

    added more reducer codes

--- frontend/src/js/redux/reducers.js
+++ frontend/src/js/redux/reducers.js
@@ -2,10 +2,17 @@ import actions from './actions';
 
 function reducer (state, action) {
     state = state || {
-            userAnswer: null
+            userAnswer: ''
         };
 
-    if (action.type === actions.SUBMIT_ANSWER) {
+    if (action.type === actions.FETCH_QUESTIONS_SUCCESS) {
+        return Object.assign({}, state, {
+            userAnswer: '',
+            questions: action.questions
+        });
+    } else if (action.type === actions.FETCH_QUESTIONS_ERROR) {
+        return state;
+    } else if (action.type === actions.SUBMIT_ANSWER) {
         return Object.assign({}, state, {
             userAnswer: action.answer
         });
--- frontend/src/js/redux/store.js
+++ frontend/src/js/redux/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 const thunk = require('redux-thunk').default;
 import reducer from './reducers';
 
-let store = createStore(reducer, compose(
+const store = createStore(reducer, compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
