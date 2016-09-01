18688a1520dfdd3f8679b9088a8d1d6b200da288
Robby Kim <robert.st.kim@gmail.com>
2016-09-01 12:07:31 -0400

    changed structure of users model and fixed questions endpoints accordingly

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -18,32 +18,28 @@ function loggedIn(req, res, next) {
 // QUESTIONS endpoint
 // will pull the first question of the question array
 // based on the user that is logged in
-router.get('/', /*loggedIn,*/ (req, res) => {
+router.get('/', (req, res) => {
   // TEMP USER
   // const userId = req.user._id;
   // console.log(userId);
-  const userId = '57c70400ed63bc78ed60634a'; // SEAN
-  // const userId = '57c7365779e5566c21c3176a'; // ROBBY
+  // const userId = '57c70400ed63bc78ed60634a'; // SEAN
+  const userId = '57c84960103a807a303f3ea3'; // ROBBY
 
   User.findById(userId, (err, user) => {
     if (err) {
       return res.status(400).json(err);
     }
 
-    Question.findById(user.questions[0].questionId, (err, question) => {
-      if (err) {
-        return res.status(400).json(err);
-      }
+    const currentQ = user.questions[0];
 
-      const resQuestion = {
-        _id: question._id,
-        question: question.question,
-        mValue: user.questions[0].mValue,
-        score: user.score,
-      };
+    const resQuestion = {
+      _id: currentQ.questionId,
+      question: currentQ.question,
+      score: user.score,
+      result: -1,
+    };
 
-      return res.status(200).json(resQuestion);
-    });
+    return res.status(200).json(resQuestion);
   });
 });
 
@@ -98,47 +94,42 @@ router.put('/', jsonParser, (req, res) => {
   }
   // TEMP USER
   // const userId = req.user._id;
-  const userId = '57c70400ed63bc78ed60634a'; // SEAN
-  // const userId = '57c7365779e5566c21c3176a'; // ROBBY
+  // const userId = '57c70400ed63bc78ed60634a'; // SEAN
+  const userId = '57c84960103a807a303f3ea3'; // ROBBY
 
   User.findById(userId, (err, user) => {
     const userQuest = user.questions.slice();
     let userScore = user.score;
     let result = false;
 
-    Question.findById(req.body._id, (err, question) => {
+    let currentQ = userQuest[0];
+
+    if (currentQ.answer === req.body.answer) {
+      currentQ.mValue *= 2;
+      userScore += 10;
+      result = true;
+    }
+
+    userQuest.shift();
+    userQuest.push(currentQ);
+    currentQ = userQuest[0];
+
+    User.findByIdAndUpdate(userId, {
+      score: userScore,
+      questions: userQuest,
+    }, { new: true }, (err, newUser) => {
       if (err) {
         return res.status(400).json(err);
       }
 
-      if (question.answer === req.body.answer) {
-        userQuest[0].mValue *= 2;
-        userScore += 10;
-        result = true;
-      }
+      const resQuestion = {
+        result,
+        _id: currentQ._id,
+        question: currentQ.question,
+        score: newUser.score,
+      };
 
-      const currentQuest = userQuest.shift();
-      userQuest.push(currentQuest);
-
-      User.findByIdAndUpdate(userId, {
-        score: userScore,
-        questions: userQuest,
-      }, { new: true }, (err, newUser) => {
-        if (err) {
-          return res.status(400).json(err);
-        }
-
-        Question.findById(newUser.questions[0].questionId, (err, newQuestion) => {
-          const resQuestion = {
-            result,
-            _id: newQuestion._id,
-            question: newQuestion.question,
-            score: newUser.score,
-          };
-
-          return res.status(200).json(resQuestion);
-        });
-      });
+      return res.status(200).json(resQuestion);
     });
   });
 });
