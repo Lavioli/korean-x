87dc41e3eda9b50a61cbb6522bb8a52f4bffe9dd
Robby Kim <robert.st.kim@gmail.com>
2016-09-02 11:16:30 -0400

    Delete google-secret.js

--- backend/google-secret.js
+++
@@ -1,5 +0,0 @@
-const GOOGLE_CLIENT_ID = '851971168921-h4gqsau1indv6nbco5ingmtnh53obb0c.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = 'rlB53hJ-07FwchHThiJh8sBl';
-
-exports.GOOGLE_CLIENT_ID = GOOGLE_CLIENT_ID;
-exports.GOOGLE_CLIENT_SECRET = GOOGLE_CLIENT_SECRET;
